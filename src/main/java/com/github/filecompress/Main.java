aaa7faed058434e41ff438fa30ff35a96defe384
jinrunheng <1175088275@qq.com>
2021-07-27 16:02:05 +0800

    finish all function

--- src/main/java/com/github/filecompress/Main.java
+++ src/main/java/com/github/filecompress/Main.java
@@ -1,7 +1,22 @@
 package com.github.filecompress;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        File testFile = new File("C:\\Users\\ASUS\\Desktop\\hello\\file-compress\\src\\main\\resources\\Harry Potter and the Sorcerer's Stone.txt");
+        List<Node> list = FileHelper.fileToHuffmanNodeList(testFile);
+        Node root = HuffmanTree.createHuffmanTree(list);
+        Map<Character, String> map = HuffmanTree.huffmanTreeNodeDataToCode(root);
+        String s = FileHelper.fileToCodeString(testFile, map);
 
+        FileHelper.writeHuffmanCodeToFile(s);
+        String s2 = FileHelper.readHuffmanFileToString();
+
+        Map<String, Character> swapMap = FileHelper.swapMapKV(map);
+        File newFile = new File("C:\\Users\\ASUS\\Desktop\\hello\\file-compress\\src\\main\\resources\\Harry Potter and the Sorcerer's Stone2.txt");
+        FileHelper.codeStringToFile(newFile,s2,swapMap);
     }
 }
